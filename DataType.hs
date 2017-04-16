55d747360cbd5c9c6da893300e08275ccc17362e
Tomislav Jovanovic <tjovanovic994@gmail.com>
2017-04-17 00:52:40 +0100

    Refactoring

---
+++ DataType.hs
@@ -0,0 +1,9 @@
+module DataType where
+
+data LispVal
+    = Atom String
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
---
+++ Parser.hs
@@ -0,0 +1,71 @@
+module Parser where
+
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import DataType
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first : rest
+    return $
+        case atom of
+            "#t" -> Bool True
+            "#f" -> Bool False
+            _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+-- parseNumber = many1 digit >>= \x -> return (Number (read x))
+
+parseList :: Parser LispVal
+parseList = List <$> sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    h <- endBy  parseExpr spaces
+    t <- char '.' >> spaces >> parseExpr
+    return $ DottedList h t
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+  <|> parseString
+  <|> parseNumber
+  <|> parseQuoted
+  <|> do char '('
+         x <- try parseList <|> parseDottedList
+         char ')'
+         return x
+
+readExpr :: String -> String
+readExpr input =
+    case parse parseExpr "lisp" input of
+        Left err -> "No match: " ++ show err
+        Right _ -> "Found value"
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
