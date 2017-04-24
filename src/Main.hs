2fa998fb98b906761d4156d176fe384b1fc8ec77
Tomislav Jovanovic <tjovanovic994@gmail.com>
2017-04-24 04:28:02 +0100

    Added repl support

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import DataType
 import Parser
 import Eval
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 import Control.Monad.Error
 
 
@@ -16,7 +17,7 @@ readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
 evalString :: String -> IO String
-evalString expr = return $ extractValue $ trapError (fmap show $ readExpr >>= eval)
+evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input =
@@ -24,12 +25,22 @@ readExpr input =
         Left err -> throwError $ Parser err
         Right val -> return val
 
+evalAndPrint :: String -> IO ()
+evalAndPrint expr =  evalString expr >>= putStrLn
 
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until_ pred prompt action
+until_ legit prompt action = do
+  result <- prompt
+  unless (legit result) $ action result >> until_ legit prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp >>> ") evalAndPrint
+
 
 main :: IO ()
 main = do
   args <- getArgs
-  let evaled = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint (head args)
+    _ -> putStrLn "Wrong input bro"
